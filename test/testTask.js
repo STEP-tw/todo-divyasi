858a3b1a0fee2176821ad80de0de3772763a081e
Divya Singh <ds492015@gmail.com>
2018-01-17 23:34:28 +0530

    all task tests are working

--- test/testTask.js
+++ test/testTask.js
@@ -4,39 +4,52 @@ const Task = require('../lib/task.js');
 
 
 describe('Title', ()=>{
-  describe('setTitle', ()=>{
-    it('should set the title', ()=>{
-      let title = new Task('todo');
-      title.setTitle('purchage');
-      assert.equal(title.title, 'purchage');
+  describe('editText', ()=>{
+    it('should edit the item', ()=>{
+      let task = new Task('purchage a earphone', 1);
+      task.editText('purchage a wrist watch');
+      assert.equal(task.todoItem, 'purchage a wrist watch');
     })
   })
-  describe('getTitle', ()=>{
-    it('should give a title', ()=>{
-      let title = new Task('todo');
-      let expected = 'todo';
-      let actual = title.getTitle();
+  describe('editStatus', ()=>{
+    it('should edit status true if item has completed', ()=>{
+      let task = new Task('purchage a earphone', 1);
+      task.editStatus(true);
+      let expected = {todoItem:'purchage a earphone', id:1, status:true};
+      let actual = task;
+      assert.deepEqual(actual, expected);
+    })
+    it('should edit status false if item has completed', ()=>{
+      let task = new Task('purchage a earphone', 1);
+      task.editStatus(false);
+      let expected = {todoItem:'purchage a earphone', id:1, status:false};
+      let actual = task;
       assert.deepEqual(actual, expected);
     })
   })
-  describe('setStatus', ()=>{
-    it('by default should set the false status', ()=>{
-      let title = new Task('todo');
-      assert.equal(title.status, false);
-    })
-  })
-  describe('setStatus', ()=>{
-    it('by default should set the true status if todo will be done', ()=>{
-      let title = new Task('todo');
-      title.setStatus(true);
-      assert.equal(title.status, true);
-    })
-  })
   describe('getStatus', ()=>{
-    it('should give status if todo will be done', ()=>{
-      let title = new Task('todo');
-      title.setStatus(true);
-      assert.equal(title.status, true);
+    it('should give the true status if status has true', ()=>{
+      let task = new Task('todo');
+      task.editStatus(true);
+      assert.equal(task.status, true);
+    })
+    it('should give the false status if status has false', ()=>{
+      let task = new Task('todo');
+      task.editStatus(false);
+      assert.equal(task.status, false);
+    })
+    it('bydefault should give the false status', ()=>{
+      let task = new Task('todo');
+      task.editStatus(false);
+      assert.equal(task.status, false);
+    })
+  })
+  describe('getText', ()=>{
+    it('should give the todoItem', ()=>{
+      let task = new Task('purchage a earphone', 1);
+      assert.equal(task.todoItem, 'purchage a earphone');
+      task.editText('purchage a wrist watch');
+      assert.equal(task.todoItem, 'purchage a wrist watch');
     })
   })
 })
